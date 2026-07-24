fd875b9098e5f6e24859a31847a593cb68a1cfa0
Felipe <ah193971@gmail.com>
2026-07-23 21:31:55 -0600

    Function pop and get added

--- src/vector.c
+++ src/vector.c
@@ -13,7 +13,7 @@ int vector_init(vector *v, size_t elem_size) {
     return 0;
 }
 
-int vector_push(vector *v, void *elem) {
+int vector_push(vector *v, void *elem) { //(A)
     if (v->length == v->capacity) {
         size_t new_cap = v->capacity == 0 ? 4 : v->capacity * 2;
         void *tmp = realloc(v->data, new_cap * v->elem_size);
@@ -27,15 +27,39 @@ int vector_push(vector *v, void *elem) {
     return 0;
 }
 
-int vector_pop(vector *v, ) {}
+int vector_pop(vector *v) {
+    if (v->length == 0) {
+        return -1;
+    }
+
+    v->length--; //(B)
+    return 0;
+}
+
+void *vector_get(vector *v, size_t i) {
+    if (i >= v->length) {
+        return NULL;
+    } else {
+        return (void *)((char *)v->data + i * v->elem_size);
+    }
+}
 
 // Condition ? value_if_true : value_if_false ternary operator
 
-// So if length and capacity are the same, multiply capacity by 2, then
-// realloc handles copying any existing data to the new block automatically.
-// Then copy the new element into the first empty slot. If the allocation
-// (tmp) fails, return -1 without touching v->data. If it succeeds, tmp is
-// assigned as the new data and new_cap is set as the new capacity. If
-// capacity and length are not equal, skip the resize and just copy the new
-// data into the next empty slot, increment length, and return 0. elem is a
-// pointer to the element the caller wants to add.
+/*
+ (A)
+ So if length and capacity are the same, multiply capacity by 2, then
+ realloc handles copying any existing data to the new block automatically.
+ Then copy the new element into the first empty slot. If the allocation
+ (tmp) fails, return -1 without touching v->data. If it succeeds, tmp is
+ assigned as the new data and new_cap is set as the new capacity. If
+ capacity and length are not equal, skip the resize and just copy the new
+ data into the next empty slot, increment length, and return 0. elem is a
+ pointer to the element the caller wants to add.
+*/
+
+/*
+ (B)
+ If you rest one in length in the next vector_push, it gets deleted without
+ doing anything
+*/
